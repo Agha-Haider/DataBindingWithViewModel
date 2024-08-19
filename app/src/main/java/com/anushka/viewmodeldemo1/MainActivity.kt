a8a2f31917282817e103a36b1127dfdc2e35ce89
AghaHaider <aghahaider662@gmail.com>
2024-08-19 11:37:29 +0500

    databindingwithlivedata

---
+++ app/src/main/java/com/anushka/viewmodeldemo1/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.anushka.viewmodeldemo1
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.anushka.viewmodeldemo1.databinding.ActivityMainBinding
+import androidx.lifecycle.Observer
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainActivityViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        binding.viewModel=viewModel
+        viewModel.count.observe(this, Observer {
+            binding.countText.text = it.toString()
+        })
+
+        binding.button.setOnClickListener {
+          //  viewModel.updateCount()
+        }
+    }
+}
